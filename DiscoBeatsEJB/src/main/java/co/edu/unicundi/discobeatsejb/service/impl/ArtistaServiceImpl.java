19d41905d08b853fd75c2c2226b7c01d40dbca99
ltramos14 <tatianarv14@gmail.com>
2021-10-25 19:08:52 -0500

    Se establece la estructura inicial ejb y war/ se realiza la conexion con la BD

---
+++ DiscoBeatsEJB/src/main/java/co/edu/unicundi/discobeatsejb/service/impl/ArtistaServiceImpl.java
@@ -0,0 +1,13 @@
+package co.edu.unicundi.discobeatsejb.service.impl;
+
+/**
+ *
+ * @author Tatiana Ramos Villanueva
+ * @author Nicolás Nieto Cárdenas
+ * @author Camilo Preciado Rojas
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class ArtistaServiceImpl {
+    
+}
